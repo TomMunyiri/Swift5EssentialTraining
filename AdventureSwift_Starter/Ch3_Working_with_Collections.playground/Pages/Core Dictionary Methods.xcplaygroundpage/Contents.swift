a293aa48b8d4c3f8564a7adfedb44b6b6ea69469
Tom Munyiri <tmmunyiri@gmail.com>
2023-02-26 12:10:01 +0300

    caching,overwriting items,nested dictionaries

--- AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Core Dictionary Methods.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Core Dictionary Methods.xcplaygroundpage/Contents.swift
@@ -16,10 +16,31 @@ Dictionary elements can be easily updated and removed using subscript syntax or 
 
  */
 // Caching and overwriting items
+var playerStats = ["HP":10,"Attack":20,"Gold":200]
+var oldValue = playerStats.updateValue(30, forKey: "Attack") //helps cache the old value while at the same time updating the value to a new value
+print(playerStats)
 
+//playerStats = ["GP":23,"Professional":56] // completely overwrites our dictionary
 
 // Caching and removing items
-
-
+//playerStats["Gold"] = nil
+var removedValue = playerStats.removeValue(forKey: "Gold")
+print(playerStats)
 // Nested dictionaries
+
+var questBoard = [
+    "Fetch Gemstones":[
+        "Objective":"Retrive 5 Gemstones",
+        "Secret":"Complete in under 5 minutes"
+    ],
+    "Defeat Big Boss":[
+        "Objective":"Beat the Boss",
+        "Secret":"Win 50% health"
+    ]
+]
+
+print(questBoard)
+
+var defeatBigBossSecret = questBoard["Defeat Big Boss"]?["Secret"]
+print(defeatBigBossSecret!)
 
