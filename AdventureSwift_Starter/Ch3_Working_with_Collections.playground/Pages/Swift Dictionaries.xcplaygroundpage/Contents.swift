b34565ac313dc46076371d3a8f58a9780ea014e3
Tom Munyiri <tmmunyiri@gmail.com>
2023-02-26 11:39:39 +0300

    creating dictionaries,adding items,accessing items,modifying existing items in dictionary

--- AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Swift Dictionaries.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Swift Dictionaries.xcplaygroundpage/Contents.swift
@@ -15,10 +15,19 @@
 
  */
 // Creating dictionaries
-
+// a dictionary stores key value pairs. Think of it as a HashMap in Java/Kotlin
+//var blackSmithShop:Dictionary<String,Int> = [:] //syntax 1 - like java/kotlin
+var blackSmithShop:[String:Int] = [:] //syntax 2 - the swift simple hand way. Here we initialize it with some empty values
+blackSmithShop["Bottle"] = 1 // adding items - very similar to kotlin
+blackSmithShop["Shield"] = 10
+print(blackSmithShop)
 
 // Accessing and modifying values
-
+var shieldPrice = blackSmithShop["Shield"]
+print("\(shieldPrice!)") //did forced unwrapping here
+blackSmithShop["Bottle"] = 24
+blackSmithShop["Toner"] = 45
 
 // All keys and values
-
+var allKeys = [String](blackSmithShop.keys)
+var allValues = [Int](blackSmithShop.values)
