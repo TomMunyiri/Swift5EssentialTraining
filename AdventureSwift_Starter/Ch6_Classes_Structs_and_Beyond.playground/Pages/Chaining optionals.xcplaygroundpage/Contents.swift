28d7761e81f53d73a84f18ea6f538f2f8828ddde
Tom Munyiri <tmmunyiri@gmail.com>
2023-05-21 18:38:23 +1100

    optional chaining

--- AdventureSwift_Starter/Ch6_Classes_Structs_and_Beyond.playground/Pages/Chaining optionals.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Starter/Ch6_Classes_Structs_and_Beyond.playground/Pages/Chaining optionals.xcplaygroundpage/Contents.swift
@@ -19,6 +19,10 @@ struct Item {
 
 struct Owner {
     var name: String
+    
+    func returnOwnerInfo()->String{
+        return "\(name) is the current owner"
+    }
 }
 
 var questDirectory = [
@@ -35,10 +39,25 @@ var questDirectory = [
 // Creating the chain
 
 var rareDagger = Item(description: "A unique dagger of unknown origin",previousOwner: nil)
+var daggerOwner = Owner(name:"The Chief Thief")
+rareDagger.previousOwner = daggerOwner
+rareDagger.previousOwner?.name = "The Old Merchant"
 //looks like the ?.let{} in kotlin
 if let owner = rareDagger.previousOwner?.name{
     print("This item used to be owned by \(owner)")
 }else{
     print("Looks like this item history is unknown")
+}
+
+if let ownerInfo = rareDagger.previousOwner?.returnOwnerInfo(){
+    print("Owner found!")
+}else{
+    print("No Owner found!")
+}
+
+if let objective = questDirectory["Fetch Gemstones"]?["Objective"]{
+    print(objective)
+}else{
+    print("No objective found")
 }
 
