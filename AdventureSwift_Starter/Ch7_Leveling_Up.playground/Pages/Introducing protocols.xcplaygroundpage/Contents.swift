792ffec934cea1baa86bafbcea83ac6b0c758a26
Tom Munyiri <tmmunyiri@gmail.com>
2023-06-28 12:16:50 +0300

    protocols

--- AdventureSwift_Starter/Ch7_Leveling_Up.playground/Pages/Introducing protocols.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Starter/Ch7_Leveling_Up.playground/Pages/Introducing protocols.xcplaygroundpage/Contents.swift
@@ -15,7 +15,40 @@
 
  */
 // Declare a protocol
+protocol Collectable{
+    var name:String {get}
+    var price:Int {get set}
+    
+    init(withName:String, startingPrice:Int)
+    func collect()->Bool
+}
 
+protocol Usable{
+    func use()
+}
 
 // Protocol adoption
 
+class Item:Collectable{
+    var name: String
+    var price: Int
+    
+    required init(withName: String, startingPrice: Int) {
+        self.name = withName
+        self.price = startingPrice
+    }
+    
+    func collect() -> Bool {
+        print("Item name is \(name)")
+        print("Item price is \(price)")
+        return true
+    }
+    
+    func use(){
+        print("Use")
+    }
+}
+
+var item = Item(withName: "Unga", startingPrice:100)
+item.collect()
+item.use()
